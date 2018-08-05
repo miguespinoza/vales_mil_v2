0323c822e9a22e47fd520a25a4891a8225fc9993
Miguel Espinoza <miguespinoza@outlook.com>
2018-08-05 15:38:26 -0500

    resgister card redux

--- app/containers/Cards/constants.js
+++ app/containers/Cards/constants.js
@@ -4,7 +4,10 @@
  *
  */
 
+const ns = con => `app/Cards/${con}`;
+
 export const DEFAULT_ACTION = 'app/Cards/DEFAULT_ACTION';
 export const REQUEST_BALANCE = 'app/Cards/REQUEST_BALANCE';
 export const REQUEST_BALANCE_ERROR = 'app/Cards/REQUEST_BALANCE_ERROR';
 export const REQUEST_BALANCE_SUCCESS = 'app/Cards/REQUEST_BALANCE_SUCCESS';
+export const REGISTER_CARD = ns`REGISTER_CARD`;
--- app/containers/Cards/saga.js
+++ app/containers/Cards/saga.js
@@ -1,22 +1,29 @@
 import { takeEvery, call, put, select } from 'redux-saga/effects';
-import { REQUEST_BALANCE } from './constants';
+import { REQUEST_BALANCE, REGISTER_CARD } from './constants';
 import CardService from 'services/CardService';
 import { makeSelectCard } from './selectors';
-import { balanceLoaded, requestBalanceError } from './actions';
+import { balanceLoaded, requestBalanceError, registerCard } from './actions';
 
 export function* getBalance(action) {
   const cardId = action.cardId;
   const card = yield select(makeSelectCard, cardId);
   try {
     const balance = yield CardService.getBalance(...card);
-    put(balanceLoaded(card, balance));
+    yield put(balanceLoaded(card, balance));
   } catch(e) {
-    put(requestBalanceError(e));
+    yield put(requestBalanceError(e));
   }
+}
+
+export function* registerCard(action) {
+  const {card} = action;
+  yield put(registerCard(card));
+
 }
 
 // Individual exports for testing
 export default function* defaultSaga() {
   // See example in containers/HomePage/saga.js
   yield takeEvery(REQUEST_BALANCE, getBalance);
+  yield takeEvery(REGISTER_CARD, registerCard);
 }
